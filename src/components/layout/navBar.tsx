6b5b69809f73355388b12d60482979612bdee8af
Divij Jain <jaindivijwork@gmail.com>
2023-08-27 14:39:56 +0530

    updated navbar

--- src/components/layout/navBar.tsx
+++ src/components/layout/navBar.tsx
@@ -16,50 +16,72 @@ import { Logo } from "../shared/icons"
 
 export const NavBar = () => {
 
-  const components: { title: string; href: string; description: string }[] = [
+  const blogs: { title: string; href: string; description: string }[] = [
     {
-      title: "Alert Dialog",
-      href: "/docs/primitives/alert-dialog",
-      description:
-        "A modal dialog that interrupts the user with important content and expects a response.",
+      title: "Lifestyle and Travel",
+      href: "/blog/lifestyle-travel",
+      description: "Globetrotting to curate life's gallery."
     },
     {
-      title: "Hover Card",
-      href: "/docs/primitives/hover-card",
-      description:
-        "For sighted users to preview content available behind a link.",
-    },
-    {
-      title: "Progress",
-      href: "/docs/primitives/progress",
-      description:
-        "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-    },
-    {
-      title: "Scroll-area",
-      href: "/docs/primitives/scroll-area",
-      description: "Visually or semantically separates content.",
-    },
-    {
-      title: "Tabs",
-      href: "/docs/primitives/tabs",
-      description:
-        "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-      title: "Tooltip",
-      href: "/docs/primitives/tooltip",
-      description:
-        "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
+      title: "Technology",
+      href: "/blog/technology",
+      description: "Navigating the digital frontier."
+    }
   ]
 
   return (
     <nav>
       <NavigationMenu>
         <NavigationMenuList>
+
+          {/* Home */}
           <NavigationMenuItem>
-            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+
+          {/* Blog */}
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Blog</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                {blogs.map((blog) => (
+                  <ListItem
+                    key={blog.title}
+                    title={blog.title}
+                    href={blog.href}
+                  >
+                    {blog.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+
+          {/* Content */}
+          <NavigationMenuItem>
+            <Link href="/content" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Content
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+
+          {/* Shop */}
+          <NavigationMenuItem>
+            <Link href="/shop" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Shop
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+
+          {/* About */}
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>About</NavigationMenuTrigger>
             <NavigationMenuContent>
               <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                 <li className="row-span-3">
@@ -91,29 +113,7 @@ export const NavBar = () => {
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                {components.map((component) => (
-                  <ListItem
-                    key={component.title}
-                    title={component.title}
-                    href={component.href}
-                  >
-                    {component.description}
-                  </ListItem>
-                ))}
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/docs" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Documentation
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+
         </NavigationMenuList>
       </NavigationMenu>
     </nav>
